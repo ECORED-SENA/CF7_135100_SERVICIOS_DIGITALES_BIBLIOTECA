a0f08adee0d34c8bd6203d6a38ce800ed089c1a0
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-10-27 14:56:58 -0500

    Avance 4

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -304,7 +304,7 @@
 
     figure(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/RtF80cqY4Os" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
